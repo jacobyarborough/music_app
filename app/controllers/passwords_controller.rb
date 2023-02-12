dace715919d3fd252ecc97a196d3fe7b7fe51145
Jacob Yarborough <jayarborough95@gmail.com>
2023-02-11 18:57:19 -0800

    Add edit password functionality

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -5,5 +5,18 @@ class PasswordsController < ApplicationController
   end 
 
   def update 
+    if Current.user.update(password_params)
+      flash[:notice] = "Successfully changed password"
+      redirect_to root_path
+    else 
+      flash[:alert] = "Something went wrong, try again"
+      redirect_to password_path
+    end
+  end 
+
+  private 
+
+  def password_params 
+    params.require(:user).permit(:password, :password_confirmation)
   end 
 end 
