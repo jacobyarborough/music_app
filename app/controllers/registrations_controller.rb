2e24de3e410808cf4cf682b4ab37d1b8a4c5d4b1
Jacob Yarborough <jayarborough95@gmail.com>
2023-02-11 17:40:41 -0800

    Add session info for user id

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,7 +6,7 @@ class RegistrationsController < ApplicationController
   def create 
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = user.id
+      session[:user_id] = @user.id
       flash[:notice] = "You have successfully created an account!"
 
       redirect_to root_path
