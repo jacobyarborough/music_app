569391897e1581fa8637f67900f114fcb0465551
Jacob Yarborough <jayarborough95@gmail.com>
2023-02-11 18:33:28 -0800

    Add sign up and sign in functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,19 @@ class SessionsController < ApplicationController
   def new
   end 
 
+  def create 
+    user = User.find_by(email: params[:email])
+
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Signed in as #{user.email}"
+      redirect_to root_path
+    else 
+      flash[:alert] = "Invalid email or password"
+      redirect_to sign_in_path
+    end 
+  end 
+
   def destroy
     session[:user_id] = nil
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
 
   get "/sign_up", to: "registrations#new"
   post "/sign_up", to: "registrations#create"
+
+  get "/sign_in", to: "sessions#new"
+  post "/sign_in", to: "sessions#create"
+
   delete "/logout", to: "sessions#destroy"
 
   get "/songs", to: "songs#index", as: :songs #keeps the name the same
