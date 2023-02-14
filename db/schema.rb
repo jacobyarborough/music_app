943c3de3b6d20010128650cad8107ea226b3b153
Jacob Yarborough <jayarborough95@gmail.com>
2023-02-14 12:04:03 -0800

    Create model and migration for playlist

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_09_163608) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_14_200233) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "playlists", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
