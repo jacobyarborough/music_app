3ce08eee8d3565441429473535b3717f5589541c
Jacob Yarborough <jayarborough95@gmail.com>
2023-02-18 22:02:44 -0800

    Add spotify account functionality

--- app/controllers/spotify_accounts_controller.rb
+++ app/controllers/spotify_accounts_controller.rb
@@ -1,5 +1,15 @@
 class SpotifyAccountsController < ApplicationController 
+  before_action :require_user_logged_in!
+
   def index 
     @spotify_accounts = Current.user.spotify_accounts
   end
+
+  def destroy 
+    spotify_account = Current.user.spotify_accounts.find(params[:id])
+    spotify_account.destroy
+
+    flash[:notice]="Account successfuly disconnected"
+    redirect_to spotify_accounts_path
+  end 
 end
